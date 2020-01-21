c56f51c863aae66b92d157151095044df19d6b1a
vijaynirmal1988 <vijaynirmal.btech@gmail.com>
2020-01-21 12:42:27 +0530

    latest 21-01-20

--- src/com/Selenium/testLccrm/testclass.java
+++ src/com/Selenium/testLccrm/testclass.java
@@ -95,76 +95,75 @@ public class testclass {
 						
 					 continue;
 					 
-					} else if(leadstatusInput.equals("Unassigned")) {
-						   // Thread.sleep(500);
-							wd.findElement(By.xpath("//*[@id='cmLeadStatusTable']/tbody/tr["+k+"]/td[1]")).click(); 
-							//Thread.sleep(1000);
-							// code for converting unassigned to inprogress
-							wd.findElement(By.id("btnAssign")).click();
-						//	Thread.sleep(1000);
-						    new Select(wd.findElement(By.id("selAsnLeadStatus"))).selectByValue(row.getCell(2,blank).getStringCellValue());
-						//    Thread.sleep(1000);
-						    new Select(wd.findElement(By.id("selAsnCounselorID"))).selectByValue(row.getCell(3,blank).getStringCellValue());
-						    wd.findElement(By.id("txtAsnRemarks")).sendKeys(row.getCell(4,blank).getStringCellValue());
-						//    Thread.sleep(1000);
-						    wd.findElement(By.xpath("//*[@id=\"viewAssignModal\"]/div/div/div/div[2]/div/div/div[1]/button")).click();
-						} else if(leadstatusInput.equals("Inprogress")) {
-							String courseInput = row.getCell(2,blank).getStringCellValue(); 
-							wd.findElement(By.xpath("//*[@id='cmLeadStatusTable']/tbody/tr["+k+"]/td[2]/a")).click(); 
-					    	Thread.sleep(2000);
+				} else if(leadstatusInput.equals("Unassigned")) {
+			 Thread.sleep(500);
+		     wd.findElement(By.xpath("//*[@id='cmLeadStatusTable']/tbody/tr["+k+"]/td[1]")).click(); 
+			 Thread.sleep(1000);
+			// code for converting unassigned to inprogress
+			 wd.findElement(By.id("btnAssign")).click();
+			 Thread.sleep(1000);
+			 new Select(wd.findElement(By.id("selAsnLeadStatus"))).selectByValue(row.getCell(2,blank).getStringCellValue());
+			 Thread.sleep(1000);
+			 new Select(wd.findElement(By.id("selAsnCounselorID"))).selectByValue(row.getCell(3,blank).getStringCellValue());
+			 wd.findElement(By.id("txtAsnRemarks")).sendKeys(row.getCell(4,blank).getStringCellValue());
+	         Thread.sleep(1000);
+			 wd.findElement(By.xpath("//*[@id=\"viewAssignModal\"]/div/div/div/div[2]/div/div/div[1]/button")).click();
+			     } else if(leadstatusInput.equals("Inprogress")) {
+			 String courseInput = row.getCell(2,blank).getStringCellValue(); 
+			 wd.findElement(By.xpath("//*[@id='cmLeadStatusTable']/tbody/tr["+k+"]/td[2]/a")).click(); 
+			 Thread.sleep(2000);
 					    	// code for converting inprogress to registered
-					    	wd.findElement(By.id("btnRegister")).click();
-					    //	Thread.sleep(2000);
-						    wd.findElement(By.id("btnAddStudent")).click();
-						 //   Thread.sleep(2000);
-						    table = wd.findElement(By.xpath("//*[@id=\"courseTable\"]"));
-						    WebDriverWait waitForElement = new WebDriverWait(wd, 30 );
-							List<WebElement> courseTable = table.findElements(By.tagName("tr"));
-						     for(int l=1;l<= courseTable.size();l++) {
-						    	
-						    String courseWeb =wd.findElement(By.xpath("//*[@id=\"courseTable\"]/tbody/tr["+l+"]/td[2]")).getText();
+			 wd.findElement(By.id("btnRegister")).click();
+			 Thread.sleep(2000);
+			 wd.findElement(By.id("btnAddStudent")).click();
+			 Thread.sleep(2000);
+			 table = wd.findElement(By.xpath("//*[@id=\"courseTable\"]"));
+		     WebDriverWait waitForElement = new WebDriverWait(wd, 30 );
+			 List<WebElement> courseTable = table.findElements(By.tagName("tr"));
+				for(int l=1;l<= courseTable.size();l++) {
+					String courseWeb =wd.findElement(By.xpath("//*[@id=\"courseTable\"]/tbody/tr["+l+"]/td[2]")).getText();
 						     if(courseInput.equals(courseWeb)) {
 						    	  wd.findElement(By.xpath("//*[@id=\"courseTable\"]/tbody/tr["+l+"]/td[1]")).click();
 						    	  break;
 						     }
 						  
 						     }
-						//    Thread.sleep(1000); 
+						    Thread.sleep(1000); 
 						    wd.findElement(By.id("btnPay")).click();
-						//    Thread.sleep(1000); 
+						    Thread.sleep(1000); 
 
 						    new Select(wd.findElement(By.id("selPaymentFor"))).selectByValue(row.getCell(3,blank).getStringCellValue());
-						//    Thread.sleep(500);
+						    Thread.sleep(500);
 						    new Select(wd.findElement(By.id("selPreferredYear"))).selectByValue(row.getCell(4,blank).getStringCellValue());
-						  //  Thread.sleep(500);
+						    Thread.sleep(500);
 						    new Select(wd.findElement(By.id("selCourseSchedulePref"))).selectByValue(row.getCell(5,blank).getStringCellValue());
-						//    Thread.sleep(500);
+						    Thread.sleep(500);
 						    new Select(wd.findElement(By.id("selExpectedCourseMonth"))).selectByValue(row.getCell(6,blank).getStringCellValue());
-						 //   Thread.sleep(500);
+						    Thread.sleep(500);
 						    new Select(wd.findElement(By.id("selModeOfPayment"))).selectByValue(row.getCell(7,blank).getStringCellValue());
-						 //   Thread.sleep(500);
+						    Thread.sleep(500);
 						    WebElement Date = wd.findElement(By.id("payDate"));
 						    Date.sendKeys(Keys.chord(Keys.CONTROL,"a")); 
 							Date.sendKeys(Keys.BACK_SPACE);
 						    Date.sendKeys(row.getCell(8,blank).getStringCellValue());
 						    Date.sendKeys(Keys.TAB); 
-						//    Thread.sleep(500);
+						    Thread.sleep(500);
 						    wd.findElement(By.id("transactionPayRef")).sendKeys(row.getCell(9,blank).getStringCellValue());
-						//    Thread.sleep(500);
+						    Thread.sleep(500);
 						    wd.findElement(By.id("feeAmount")).sendKeys(row.getCell(10,blank).getStringCellValue());
 						    wd.findElement(By.id("btnSubmitPayment")).click();
-						 //   Thread.sleep(500);
+						    Thread.sleep(500);
 						    wd.findElement(By.id("btnRegisterCancel")).click();
-						//    Thread.sleep(4000); 
+						    Thread.sleep(4000); 
 						  // move to dashboard page
 							wd.findElement(By.xpath("//text()[contains(.,'Leads')]/ancestor::p[1]")).click(); 
 							wd.findElement(By.xpath("//p[text()='Dashboard']")).click(); 
-						//	Thread.sleep(2000);
+							Thread.sleep(2000);
 
 							new Select(wd.findElement(By.name("cmLeadStatusTable_length"))).selectByValue("25");
 
-				} else {
-					continue;
+			         	} else {
+				            	continue;
 								}
 				 			} else {
 				 				continue;
@@ -172,7 +171,6 @@ public class testclass {
 
 						}		
 				WebElement element = wd.findElement(By.tagName("body"));
-
 				JavascriptExecutor js = (JavascriptExecutor)wd;
 				js.executeScript("arguments[0].scrollIntoView();", element); 
 					
